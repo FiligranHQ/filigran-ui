8e0b5ecb4e1dc7eb6c5debe401d2b48280f68fa1
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-05-03 13:46:02 +0200

    [Library] Filigran-icon add Icon at the end of the React Component

--- packages/filigran-icon/index-template.js
+++ packages/filigran-icon/index-template.js
@@ -3,7 +3,7 @@ const path = require('path');
 function defaultIndexTemplate(filePaths) {
   const exportEntries = filePaths.map(({ path: filePath, originalPath }) => {
     const basename = path.basename(filePath, path.extname(filePath));
-    const componentName = /^\d/.test(basename) ? `Svg${basename}` : basename;
+    const componentName = /^\d/.test(basename) ? `Svg${basename}Icon` : `${basename}Icon`;
     return `export const ${componentName} = lazy(() => import('./${basename}'))`;
   });
   return [`import { lazy } from 'react';`,...exportEntries].join('\n');
