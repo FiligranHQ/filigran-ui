015fca9fdbcc7d59acb790dd233f18667d7176c1
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-05-21 13:01:18 +0200

    [filigran-ui] Add Filigran colors

--- projects/filigran-website/app/layout.tsx
+++ projects/filigran-website/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next'
+import 'filigran-ui/theme.css'
 import './globals.css'
 import {ThemeProvider} from '@/components/providers'
 import {SiteHeader} from '@/components/site-header'
