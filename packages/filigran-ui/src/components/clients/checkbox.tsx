47c0c4f8097976a5ec7ba3f8ddc5ff65f6853965
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-08-23 15:24:31 +0200

    [filigran-ui] Remove console.log in checkbox

--- packages/filigran-ui/src/components/clients/checkbox.tsx
+++ packages/filigran-ui/src/components/clients/checkbox.tsx
@@ -9,31 +9,28 @@ import {CheckBoldIcon, CheckIndeterminateSmallBoldIcon} from 'filigran-icon'
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({className, checked, ...props}, ref) => {
-  console.log(props)
-  return (
-    <CheckboxPrimitive.Root
-      ref={ref}
+>(({className, checked, ...props}, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      'peer h-[1.125rem] w-[1.125rem] shrink-0 rounded-md border border-border-neutral ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      className
+    )}
+    checked={checked}
+    {...props}>
+    <CheckboxPrimitive.Indicator
       className={cn(
-        'peer h-[1.125rem] w-[1.125rem] shrink-0 rounded-md border border-border-neutral ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
-        className
+        'flex h-full flex-1 items-center justify-center text-current'
+      )}>
+      {/* In case the checked is not controlled, the checked props is undefined */}
+      {checked === undefined && <CheckBoldIcon className="w-[0.625rem]" />}
+      {checked === true && <CheckBoldIcon className="w-[0.625rem]" />}
+      {checked === 'indeterminate' && (
+        <CheckIndeterminateSmallBoldIcon className="w-2 text-border-neutral" />
       )}
-      checked={checked}
-      {...props}>
-      <CheckboxPrimitive.Indicator
-        className={cn(
-          'flex h-full flex-1 items-center justify-center text-current'
-        )}>
-        {/* In case the checked is not controlled, the checked props is undefined */}
-        {checked === undefined && <CheckBoldIcon className="w-[0.625rem]" />}
-        {checked === true && <CheckBoldIcon className="w-[0.625rem]" />}
-        {checked === 'indeterminate' && (
-          <CheckIndeterminateSmallBoldIcon className="w-2 text-border-neutral" />
-        )}
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-  )
-})
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+))
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export {Checkbox}
