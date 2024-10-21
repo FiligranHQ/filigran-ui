783b3677764df417fc846bc5448c7f9bac87987a
thervy <tiffaine.hervy@filigran.io>
2024-10-21 11:14:52 +0200

    [fiel-input]: Add the fileType handling

--- projects/filigran-website/components/example/example-file-input.tsx
+++ projects/filigran-website/components/example/example-file-input.tsx
@@ -3,23 +3,23 @@ import React, {useRef, useState} from 'react'
 import {FileInput} from 'filigran-ui/clients'
 const ExampleFileInput = () => {
   const fileInputRef = useRef(null)
-  const [fileName, setFileName] = useState<string | null>(null)
 
   const handleFileChange = (files: FileList | null) => {
     if (files && files.length > 0) {
       console.log('Fichier sélectionné :', files[0])
-      setFileName(files[0].name)
     }
   }
 
   return (
     <div className="my-8">
       <FileInput
+        allowedTypes={
+          'image/*,application/pdf,video/*,application/msword,application/zip,application/json'
+        }
         handleFileChange={handleFileChange}
         ref={fileInputRef}
         texts={{
-          dragUnactive: 'Select a fileX',
-          fileOk: fileName ?? '',
+          selectFile: 'Select a fileX',
         }}
       />
     </div>
