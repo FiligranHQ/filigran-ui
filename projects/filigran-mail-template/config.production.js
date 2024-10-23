e69fb4e952015d319953b4d03c3ffe9ba04676a0
jpkha <jean-philippe.kha@filigran.io>
2024-10-23 15:19:19 +0200

    Delete projects/filigran-website/content/docs/components/sheet.mdx~

---
+++ projects/filigran-mail-template/config.production.js
@@ -0,0 +1,25 @@
+/*
+|-------------------------------------------------------------------------------
+| Production config                       https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| This is where you define settings that optimize your emails for production.
+| These will be merged on top of the base config.js, so you only need to
+| specify the options that are changing.
+|
+*/
+
+/** @type {import('@maizzle/framework').Config} */
+module.exports = {
+  build: {
+    templates: {
+      destination: {
+        path: 'build_production',
+      },
+    },
+  },
+  inlineCSS: true,
+  removeUnusedCSS: true,
+  shorthandCSS: true,
+  prettify: true,
+}
---
+++ projects/filigran-mail-template/tailwind.config.js
@@ -0,0 +1,4 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require("tailwindcss-preset-email")],
+};
