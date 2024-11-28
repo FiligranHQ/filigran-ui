fdc2d95fd68fadbf848c43b3dc0e9fee4c213607
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-11-28 15:47:16 +0100

    [filigran-email] Fix email for Outlook and Gmail

--- packages/filigran-email/tailwind.config.js
+++ packages/filigran-email/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [
-    require('tailwindcss-preset-email'),
-  ],
-}
+  presets: [require("tailwindcss-preset-email")],
+  theme: {
+    extend: {
+      colors: {
+        background: "#F1F5F9",
+        foreground: "#00020c",
+      },
+    },
+  },
+};
