c72c9390bf6cb0db161af03c49a306b4e488d4f2
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2025-01-20 11:36:21 +0100

    [filigran-ui] Add auto-form

---
+++ packages/filigran-ui/src/components/auto-form/auto-form.tsx
@@ -0,0 +1,131 @@
+'use client'
+import {
+  type DefaultValues,
+  type FormState,
+  useForm,
+  type UseFormReturn,
+} from 'react-hook-form'
+import {z} from 'zod'
+
+import {zodResolver} from '@hookform/resolvers/zod'
+
+import {useEffect} from 'react'
+import {cn} from '../../lib/utils'
+import {Form} from '../clients'
+import {Button} from '../servers'
+import AutoFormObject from './fields/object'
+import type {Dependency, FieldConfig} from './types'
+import {
+  getDefaultValues,
+  getObjectFormSchema,
+  type ZodObjectOrWrapped,
+} from './utils'
+
+const AutoFormSubmit = ({
+  children,
+  className,
+  disabled,
+}: {
+  children?: React.ReactNode
+  className?: string
+  disabled?: boolean
+}) => {
+  return (
+    <Button
+      type="submit"
+      disabled={disabled}
+      className={className}>
+      {children ?? 'Submit'}
+    </Button>
+  )
+}
+
+const AutoForm = <SchemaType extends ZodObjectOrWrapped>({
+  formSchema,
+  values: valuesProp,
+  onValuesChange: onValuesChangeProp,
+  onParsedValuesChange,
+  onSubmit: onSubmitProp,
+  fieldConfig,
+  children,
+  className,
+  dependencies,
+}: {
+  formSchema: SchemaType
+  values?: Partial<z.infer<SchemaType>>
+  onValuesChange?: (
+    values: Partial<z.infer<SchemaType>>,
+    form: UseFormReturn<z.infer<SchemaType>>
+  ) => void
+  onParsedValuesChange?: (
+    values: Partial<z.infer<SchemaType>>,
+    form: UseFormReturn<z.infer<SchemaType>>
+  ) => void
+  onSubmit?: (
+    values: z.infer<SchemaType>,
+    form: UseFormReturn<z.infer<SchemaType>>
+  ) => void
+  fieldConfig?: FieldConfig<z.infer<SchemaType>>
+  children?:
+    | React.ReactNode
+    | ((formState: FormState<z.infer<SchemaType>>) => React.ReactNode)
+  className?: string
+  dependencies?: Dependency<z.infer<SchemaType>>[]
+}) => {
+  const objectFormSchema = getObjectFormSchema(formSchema)
+  const defaultValues: DefaultValues<z.infer<typeof objectFormSchema>> | null =
+    getDefaultValues(objectFormSchema, fieldConfig)
+
+  const form = useForm<z.infer<typeof objectFormSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues ?? undefined,
+    values: valuesProp,
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const parsedValues = formSchema.safeParse(values)
+    if (parsedValues.success) {
+      onSubmitProp?.(parsedValues.data, form)
+    }
+  }
+
+  useEffect(() => {
+    const subscription = form.watch((values) => {
+      onValuesChangeProp?.(values, form)
+      const parsedValues = formSchema.safeParse(values)
+      if (parsedValues.success) {
+        onParsedValuesChange?.(parsedValues.data, form)
+      }
+    })
+
+    return () => subscription.unsubscribe()
+  }, [form, formSchema, onValuesChangeProp, onParsedValuesChange])
+
+  const renderChildren =
+    typeof children === 'function'
+      ? children(form.formState as FormState<z.infer<SchemaType>>)
+      : children
+
+  return (
+    <div className="w-full">
+      <Form {...form}>
+        <form
+          onSubmit={(e) => {
+            form.handleSubmit(onSubmit)(e)
+          }}
+          className={cn('space-y-5', className)}>
+          <AutoFormObject
+            schema={objectFormSchema}
+            form={form}
+            dependencies={dependencies}
+            fieldConfig={fieldConfig}
+          />
+
+          {renderChildren}
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export {AutoForm, AutoFormSubmit}
---
+++ packages/filigran-ui/src/components/clients/date-picker.tsx
@@ -0,0 +1,43 @@
+'use client'
+import {format} from 'date-fns'
+
+import {forwardRef} from 'react'
+import {cn} from '../../lib/utils'
+import {Button} from '../servers'
+import {Calendar} from './calendar'
+import {Popover, PopoverContent, PopoverTrigger} from './popover'
+import {CalendarMonthIcon} from 'filigran-icon'
+
+export const DatePicker = forwardRef<
+  HTMLDivElement,
+  {
+    date?: Date
+    setDate: (date?: Date) => void
+  }
+>(function DatePickerCmp({date, setDate}, ref) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-full justify-start text-left font-normal normal-case',
+            !date && 'text-muted-foreground'
+          )}>
+          <CalendarMonthIcon className="mr-2 h-3 w-3" />
+          {date ? format(date, 'PP') : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className="w-auto p-0"
+        ref={ref}>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+})
