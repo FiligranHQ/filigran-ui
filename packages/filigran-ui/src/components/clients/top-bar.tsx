9b78dee3268c5063ee79299c1cf44bf42df2d757
Adrien Servel <adrien.servel@filigran.io>
2025-02-07 13:19:07 +0100

    WORKING

---
+++ packages/filigran-ui/src/components/clients/top-bar.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { cn } from '../../lib/utils';
+import { SearchInput } from './search-input';
+import { IndividualIcon } from 'filigran-icon';
+
+export const TopBar = ({ displayLogo = true }) => (
+  <header
+    className={cn(
+      'sticky top-0 z-10 flex h-14 w-full flex-shrink-0 items-center border-b bg-page-background dark:bg-background px-4 justify-between',
+      displayLogo ? '' : 'sm:justify-end'
+    )}>
+    <SearchInput />
+    <div className={'flex flex-1'} />
+    <div className={"size-7"}>
+      <IndividualIcon />
+    </div>
+  </header>
+);
