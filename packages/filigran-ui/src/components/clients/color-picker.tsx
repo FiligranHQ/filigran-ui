fddb33aeed22ace960b12b3ae141775b8be6a3af
Adrien Servel <50013774+Kedae@users.noreply.github.com>
2025-02-17 11:00:54 +0100

    [filigran-ui][color-picker] Add of color picker input

---
+++ packages/filigran-ui/src/components/clients/color-picker.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { forwardRef, useMemo, useState } from 'react';
+import { SketchPicker } from 'react-color'
+import { cn } from '../../lib/utils';
+import { Button, type ButtonProps, Input } from '../servers';
+import { Popover, PopoverContent, PopoverTrigger } from './popover';
+
+interface ColorPickerProps {
+  value: string;
+  onChange: (value: string) => void;
+  onBlur?: () => void;
+}
+
+const ColorPicker = forwardRef<HTMLInputElement,
+  Omit<ButtonProps, 'value' | 'onChange' | 'onBlur'> & ColorPickerProps>(
+  (
+    { disabled, value, onChange, onBlur, name, className, ...props },
+    forwardedRef
+  ) => {
+    const [open, setOpen] = useState(false);
+
+    const parsedValue = useMemo(() => {
+      return value || '#FFFFFF';
+    }, [value]);
+
+    return (
+      <Popover onOpenChange={setOpen} open={open}>
+        <div className="flex gap-2 items-center relative">
+          <Input
+            maxLength={7}
+            onChange={(e) => {
+              onChange(e?.currentTarget?.value);
+            }}
+            ref={forwardedRef}
+            value={parsedValue}
+          />
+          <PopoverTrigger asChild disabled={disabled} onBlur={onBlur}>
+            <Button
+              {...props}
+              className={cn('block', className, "size-5 absolute right-2")}
+              name={name}
+              onClick={() => {
+                setOpen(true);
+              }}
+              size="icon-rounded"
+              style={{
+                backgroundColor: parsedValue,
+              }}
+              variant="outline"
+            >
+              <div />
+            </Button>
+          </PopoverTrigger>
+        </div>
+        <PopoverContent className="w-full">
+          <SketchPicker color={parsedValue} onChangeComplete={(v: { hex: string }) => onChange(v.hex)} />
+        </PopoverContent>
+      </Popover>
+    );
+  }
+);
+ColorPicker.displayName = 'ColorPicker';
+
+export { ColorPicker };
