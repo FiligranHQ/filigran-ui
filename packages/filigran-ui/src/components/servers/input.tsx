44a689550b90aacbc96817d0f6e93a0de3cfdc40
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2025-03-12 18:07:52 +0100

    [filigran-ui] Improve input component by adding potentially icon

--- packages/filigran-ui/src/components/servers/input.tsx
+++ packages/filigran-ui/src/components/servers/input.tsx
@@ -1,24 +1,46 @@
-import * as React from 'react'
+import * as React from "react";
+
+import type {ReactNode} from 'react'
 import {cn} from '../../lib/utils'
+import {SearchIcon} from 'filigran-icon'
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({className, type, ...props}, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-9 w-full rounded border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = 'Input'
+  ({ className, type, startIcon, endIcon, ...props }, ref) => {
 
-export {Input}
+
+    return (
+      <div className="w-full relative">
+        {startIcon && (
+          <div className="absolute left-s top-1/2 transform -translate-y-1/2">
+            {startIcon}
+          </div>
+        )}
+        <input
+          type={type}
+          className={cn(
+            'flex h-9 w-full rounded border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+            startIcon ? "pl-xxl" : "",
+            endIcon ? "pr-xxl" : "",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {endIcon && (
+          <div className="absolute right-s top-1/2 transform -translate-y-1/2">
+            {endIcon}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
