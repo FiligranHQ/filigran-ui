960d08d183fb329dcb46140134169619731aef22
thervy <tiffaine.hervy@filigran.io>
2025-09-05 11:15:36 +0200

    [filigran-ui] - (Combobox): Add reset icon

--- packages/filigran-ui/src/components/clients/combobox.tsx
+++ packages/filigran-ui/src/components/clients/combobox.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { CheckIcon, KeyboardArrowDownIcon } from 'filigran-icon'
+import {CheckIcon, CloseIcon, KeyboardArrowDownIcon} from 'filigran-icon'
 import * as React from 'react'
-import { cn } from '../../lib/utils'
-import { Button } from '../servers'
+import {cn} from '../../lib/utils'
+import {Button} from '../servers'
 import {
   Command,
   CommandEmpty,
@@ -12,7 +12,7 @@ import {
   CommandItem,
   CommandList,
 } from './command'
-import { Popover, PopoverContent, PopoverTrigger } from './popover'
+import {Popover, PopoverContent, PopoverTrigger} from './popover'
 
 export interface ComboboxItem {
   value: string
@@ -24,7 +24,7 @@ interface ComboboxProps<T> {
   order: string
   placeholder: string
   emptyCommand: string
-  onValueChange: (value: (T ) | undefined) => void
+  onValueChange: (value: T | undefined) => void
   onInputChange?: (value: string) => void
   value?: T
   className?: string
@@ -33,49 +33,69 @@ interface ComboboxProps<T> {
 }
 
 function Combobox<T>({
-                       dataTab,
-                       order,
-                       placeholder,
-                       emptyCommand,
-                       onValueChange,
-                       onInputChange,
-                       value,
-                       className,
-                       keyLabel = 'label',
-                       keyValue = 'value'
-                     }: ComboboxProps<T>) {
+  dataTab,
+  order,
+  placeholder,
+  emptyCommand,
+  onValueChange,
+  onInputChange,
+  value,
+  className,
+  keyLabel = 'label',
+  keyValue = 'value',
+}: ComboboxProps<T>) {
   const [open, setOpen] = React.useState(false)
 
   const handleSelect = (selectedValue: string) => {
-    const selectedItem = dataTab.find(
-      (item) => String(item[keyValue as keyof typeof item]) === selectedValue
-    ) || undefined
+    const selectedItem =
+      dataTab.find(
+        (item) => String(item[keyValue as keyof typeof item]) === selectedValue
+      ) || undefined
 
     setOpen(false)
     onValueChange(selectedItem)
   }
 
-  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     onInputChange && onInputChange(event.target.value)
+  }
+
+  const handleReset = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    onValueChange(undefined)
   }
 
   // @ts-ignore
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover
+      open={open}
+      onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className={cn('normal-case w-full justify-between border-input', className)}
-          onClick={() => setOpen(!open)}
-        >
+          className={cn(
+            'normal-case w-full justify-between border-input',
+            className
+          )}
+          onClick={() => setOpen(!open)}>
           {value ? (
             String(value[keyLabel as keyof T])
           ) : (
             <span className="text-muted-foreground">{order}</span>
           )}
-          <KeyboardArrowDownIcon className="h-2.5 w-2.5 cursor-pointer text-muted-foreground" />
+          <div className="flex items-center gap-s">
+            {value && (
+              <CloseIcon
+                className="h-2.5 w-2.5 cursor-pointer text-muted-foreground"
+                onClick={handleReset}
+              />
+            )}
+            <KeyboardArrowDownIcon className="h-2.5 w-2.5 cursor-pointer text-muted-foreground" />
+          </div>
         </Button>
       </PopoverTrigger>
       <PopoverContent className="p-0 popover-content-width-same-as-its-trigger">
@@ -88,12 +108,14 @@ function Combobox<T>({
                 <CommandItem
                   key={String(data[keyValue as keyof T])}
                   value={String(data[keyValue as keyof T])}
-                  onSelect={() => handleSelect(String(data[keyValue as keyof T]))}
-                >
+                  onSelect={() =>
+                    handleSelect(String(data[keyValue as keyof T]))
+                  }>
                   <CheckIcon
                     className={cn(
                       'mr-2 h-4 w-4',
-                      (value as ComboboxItem)?.value === data[keyValue as keyof T]
+                      (value as ComboboxItem)?.value ===
+                        data[keyValue as keyof T]
                         ? 'opacity-100'
                         : 'opacity-0'
                     )}
@@ -111,4 +133,4 @@ function Combobox<T>({
   )
 }
 
-export { Combobox }
+export {Combobox}
