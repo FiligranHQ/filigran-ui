f668ec81e9bf6ea767138d07295364d5e82a8d19
hervyt <tiffaine.hervy@filigran.io>
2026-02-18 16:19:26 +0100

    [filigran-ui] - (button): update focus colo with button color (#212)

--- packages/filigran-ui/src/components/servers/button.tsx
+++ packages/filigran-ui/src/components/servers/button.tsx
@@ -3,33 +3,30 @@ import {cva, type VariantProps} from 'class-variance-authority'
 import * as React from 'react'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded font-normal text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded font-normal text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/75',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/75 focus-visible:ring-primary/50',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/75',
+          'bg-destructive text-destructive-foreground hover:bg-destructive/75 focus-visible:ring-destructive/50',
         outline: 'border border-border-medium bg-transparent hover:bg-hover',
-        'outline-primary':
-          'border border-primary bg-transparent hover:bg-hover text-primary',
-        'outline-destructive':
-          'border border-destructive/75 bg-transparent hover:bg-hover text-destructive',
-        'outline-secondary':
-          'border border-secondary bg-transparent hover:bg-hover text-secondary',
+        'outline-primary': 'border border-primary bg-transparent hover:bg-hover text-primary focus-visible:ring-primary/50',
+        'outline-destructive': 'border border-destructive/75 bg-transparent hover:bg-hover text-destructive focus-visible:ring-destructive/50',
+        'outline-secondary': 'border border-secondary bg-transparent hover:bg-hover text-secondary focus-visible:ring-secondary/50',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/75',
+          'bg-secondary text-secondary-foreground hover:bg-secondary/75 focus-visible:ring-secondary/50',
         ghost: 'hover:bg-hover',
-        'ghost-primary': 'hover:bg-hover text-primary',
-        'ghost-destructive': 'hover:bg-hover text-destructive',
-        'ghost-secondary': 'hover:bg-hover text-secondary',
+        'ghost-primary': 'hover:bg-hover text-primary focus-visible:ring-primary/50',
+        'ghost-destructive': 'hover:bg-hover text-destructive focus-visible:ring-destructive/50',
+        'ghost-secondary': 'hover:bg-hover text-secondary focus-visible:ring-secondary/50',
         link: 'text-primary underline-offset-4 hover:underline normal-case',
         'link-primary':
-          'text-primary underline-offset-4 hover:underline normal-case',
+          'text-primary underline-offset-4 hover:underline normal-case focus-visible:ring-primary/50',
         'link-destructive':
-          'text-destructive underline-offset-4 hover:underline normal-case',
+          'text-destructive underline-offset-4 hover:underline normal-case focus-visible:ring-destructive/50',
         'link-secondary':
-          'text-secondary underline-offset-4 hover:underline normal-case',
+          'text-secondary underline-offset-4 hover:underline normal-case focus-visible:ring-secondary/50',
       },
       size: {
         default: 'h-9 px-4 py-2',
@@ -45,6 +42,7 @@ const buttonVariants = cva(
     },
   }
 )
+
 
 import {cn} from '../../lib/utils'
 
