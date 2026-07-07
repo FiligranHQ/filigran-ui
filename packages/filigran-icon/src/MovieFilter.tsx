855fc498bb78e51291474c941f0ecd128a2fa186
Julien Gérard <julien.gerard@filigran.io>
2026-07-07 15:32:34 +0200

    [filigran-icon] add resource type icons (insert-chart, insights, library-books, extension, movie-filter)

---
+++ packages/filigran-icon/src/MovieFilter.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from "react";
+import type { SVGRProps } from "../model/svgr";
+const SvgMovieFilter = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    role="img"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d="m10 11-.94 2.06L7 14l2.06.94L10 17l.94-2.06L13 14l-2.06-.94zm8.01-7 2 4h-3l-2-4h-2l2 4h-3l-2-4h-2l2 4h-3l-2-4h-1c-1.1 0-1.99.9-1.99 2l-.01 12c0 1.1.9 2 2 2h16c1.1 0 1.99-.9 1.99-2V4zm2 14h-16V6.47L5.77 10H16l-.63 1.37L14 12l1.37.63L16 14l.63-1.37L18 12l-1.37-.63L16 10h4.01z" />
+  </svg>
+);
+export default SvgMovieFilter;
