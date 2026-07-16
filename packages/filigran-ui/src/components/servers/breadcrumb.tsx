72077c949e4c3f428828ebf5371565d9e021b3bf
jpkha <jean-philippe.kha@filigran.io>
2026-07-16 15:52:34 +0200

    feat(revamp): fix feedback from product

--- packages/filigran-ui/src/components/servers/breadcrumb.tsx
+++ packages/filigran-ui/src/components/servers/breadcrumb.tsx
@@ -55,7 +55,7 @@ const BreadcrumbLink = React.forwardRef<
   return (
     <Comp
       ref={ref}
-      className={cn('transition-colors text-primary', className)}
+      className={cn('transition-colors', className)}
       {...props}
     />
   )
