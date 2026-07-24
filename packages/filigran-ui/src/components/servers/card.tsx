b9967ed659fedec3bb2f1258d1ad78605f362a37
hervyt <tiffaine.hervy@filigran.io>
2026-07-24 16:45:10 +0200

    fix(cardandbuttongradient): improve styles (#367)

--- packages/filigran-ui/src/components/servers/card.tsx
+++ packages/filigran-ui/src/components/servers/card.tsx
@@ -8,7 +8,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-lg border border-border bg-card text-card-foreground shadow-xs',
+      'rounded-lg bg-elevation-surface-highlight text-text-default-primary',
       className
     )}
     {...props}
@@ -34,10 +34,7 @@ const CardTitle = React.forwardRef<
 >(({className, ...props}, ref) => (
   <h3
     ref={ref}
-    className={cn(
-      'text-2xl font-semibold leading-none tracking-tight',
-      className
-    )}
+    className={cn(className)}
     {...props}
   />
 ))
@@ -49,7 +46,7 @@ const CardDescription = React.forwardRef<
 >(({className, ...props}, ref) => (
   <p
     ref={ref}
-    className={cn('text-sm text-muted-foreground', className)}
+    className={cn('text-content-body-base', className)}
     {...props}
   />
 ))
